922e55350caf06e609398ab68b584f5b8053dc44
Serg4356 <asdfg111000@yandex.ru>
2019-04-10 18:59:19 +0500

    rewrite all program

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import asyncio
 import curses
 import random
 
-
+'''
 def draw(canvas):
     row, column = (5, 20)
     canvas.addstr(row, column, 'Hello, World!', curses.A_REVERSE | curses.A_BLINK)
@@ -59,7 +59,7 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
         canvas.addstr(round(row), round(column), ' ')
         row += rows_speed
         column += columns_speed
-
+'''
 
 def choose_star():
   stars = ['+', ':', '*', '.']
@@ -70,93 +70,53 @@ def choose_coords(maxx, maxy):
   return random.randint(1, maxx-1), random.randint(1, maxy-1)
 
 
-async def blink(canvas, row, column, symbol='*'):
-    while True:
-        canvas.addstr(row, column, symbol, curses.A_DIM)
-        await Sleep(2)
-
-        canvas.addstr(row, column, symbol)
-        await Sleep(0.3)
-
-        canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await Sleep(0.5)
-
-        canvas.addstr(row, column, symbol)
-        await Sleep(0.3)
-
-
 def main(canvas):
+    canvas.border()
     curses.curs_set(False)
     stars_count = 100
     coroutines = []
-    canvas_width, canvas_height = canvas.getmaxyx()
-    for star in range(stars_count):
-        row, column = choose_coords(canvas_width, canvas_height)
-        coroutines.append(blink(canvas, row, column, symbol=choose_star()))
-    iterations_before_change_brightness = [0]*(stars_count)
-    gun_shot = fire(canvas, 18, 30)
-    #coroutines.append(gun_shot)
-    while True:
-      try:
-        for num, coroutine in enumerate(coroutines):
-          if iterations_before_change_brightness[num] <= 0:
-            #try:
-             # seconds_to_sleep = coroutines[random.randint(0,len(coroutines)-1)].send(None).seconds
-            #except AttributeError:
-              #seconds_to_sleep = 1
-            seconds_to_sleep = coroutines[random.randint(0,len(coroutines)-1)].send(None).seconds
-            ticks_to_sleep = convert_seconds_to_iterations(
-              seconds_to_sleep
-            )
-            iterations_before_change_brightness[num] = ticks_to_sleep
-          gun_shot.send(None)
-          iterations_before_change_brightness[num] -= 1
-      except StopIteration:
-            coroutines.remove(coroutine)
-      if len(coroutines) == 0:
-        break
-      canvas.refresh()
+    max_row, max_column = canvas.getmaxyx()
+    coroutine = blink(canvas, 5, 5)
+    for i in range(stars_count):
+        coroutines.append(blink(
+            canvas,
+            random.randint(2, max_row-2),
+            random.randint(2, max_column-2),
+            symbol=choose_star()
+        ))
+    x = 0
+    while x<10000:
+        x += 1
+        try:
+            coroutine_num = random.randint(0,len(coroutines)-1)
+            coroutines[coroutine_num].send(None)
+        #for coroutine in coroutines:
+            #coroutine.send(None)
+        except StopIteration:
+            coroutines.remove(coroutines[coroutine_num])
+        canvas.refresh()
+        time.sleep(0.01)
 
 
-class EventLoopCommand():
-
-    def __await__(self):
-        return (yield self)
-
-
-class Sleep(EventLoopCommand):
-
-  def __init__(self, seconds):
-      self.seconds = seconds
-
-
-def convert_seconds_to_iterations(seconds):
-  return seconds * 10000
-
-async def blink_2(canvas, row, column, symbol='*'):
-    while True:
+async def blink(canvas, row, column, symbol='*'):
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for iter in range(10):
+        for _ in range(10):
             await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        for _ in range(3):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await asyncio.sleep(0)
+        for _ in range(5):
+            await asyncio.sleep(0)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
-
-
-def main_2(canvas):
-    curses.curs_set(False)
+        for _ in range(3):
+            await asyncio.sleep(0)
 
 
 
 if __name__ == '__main__':
     curses.update_lines_cols()
     curses.wrapper(main)
-    #curses.wrapper(draw)
-    #while True:
-    #curses.wrapper(draw_star)
