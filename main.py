6ec33019384fa612e8c7d6c6b922eb1ae3a7822e
Serg4356 <asdfg111000@yandex.ru>
2019-04-18 16:30:47 +0500

    fix bugs

--- main.py
+++ main.py
@@ -7,77 +7,37 @@ import sys
 from curses_tools import read_controls, draw_frame, get_frame_size
 
 
-'''
-def draw(canvas):
-    row, column = (5, 20)
-    canvas.addstr(row, column, 'Hello, World!', curses.A_REVERSE | curses.A_BLINK)
-    canvas.border()
-    curses.curs_set(False)
-    canvas.refresh()
-    time.sleep(4)
-
-
-def draw_star(canvas):
-    curses.curs_set(False)
-    row, column = (3, 10)
-    canvas.addstr(row, column, '*', curses.A_DIM)
-    time.sleep(2)
-    canvas.refresh()
-    canvas.addstr(row, column, '*')
-    time.sleep(0.3)
-    canvas.refresh()
-    canvas.addstr(row, column, '*', curses.A_BOLD)
-    time.sleep(0.5)
-    canvas.refresh()
-    canvas.addstr(row, column, '*')
-    time.sleep(0.3)
-    canvas.refresh()
-'''
-
 
 def get_frame(file_path):
     with open(file_path, 'r') as file:
         return file.read()
 
 
+def is_crossing_border(current_position ,shift_direction,
+                       frame_length, canvas_size):
+    space_near_border = 1
+    critical_position = canvas_size - frame_length - space_near_border
+    return ((current_position < critical_position) and (shift_direction > 0))\
+           or ((current_position > space_near_border) and (shift_direction < 0))
+
+
 async def animate_spaceship(canvas, frame_1, frame_2, row, column):
     while True:
-        canvas.nodelay(True)
-        maxx, maxy = canvas.getmaxyx()
-        rows_direction, columns_direction, space_direction = read_controls(canvas)
-        frame_1_rows, frame_1_columns = get_frame_size(frame_1)
+        frames = [frame_1, frame_2]
+        for frame in frames:
+            maxx, maxy = canvas.getmaxyx()
+            rows_direction, columns_direction, space_direction = read_controls(canvas)
+            frame_rows, frame_columns = get_frame_size(frame)
 
-        if (row <= maxx - frame_1_rows - 3) & (rows_direction > 0):
-            row += rows_direction
-        elif (row >= 2) & (rows_direction < 0):
-            row += rows_direction
+            if is_crossing_border(row, rows_direction, frame_rows, maxx):
+                row += rows_direction
+            if is_crossing_border(column, columns_direction, frame_columns, maxy):
+                column += columns_direction
 
-        if (column <= maxy - frame_1_columns - 3) & (columns_direction > 0):
-            column += columns_direction
-        elif (column > 2) & (columns_direction < 0):
-            column += columns_direction
+            draw_frame(canvas, row, column, frame)
+            await asyncio.sleep(0)
 
-        draw_frame(canvas, row, column, frame_1)
-        await asyncio.sleep(0)
-
-        draw_frame(canvas, row, column, frame_1, negative=True)
-
-        rows_direction, columns_direction, space_direction = read_controls(canvas)
-        maxx, maxy = canvas.getmaxyx()
-        frame_2_rows, frame_2_columns = get_frame_size(frame_2)
-        if (row <= maxx - frame_2_rows - 3) & (rows_direction > 0):
-            row += rows_direction
-        elif (row >= 2) & (rows_direction < 0):
-            row += rows_direction
-
-        if (column <= maxy - frame_2_columns - 3) & (columns_direction > 0):
-            column += columns_direction
-        elif (column > 2) & (columns_direction < 0):
-            column += columns_direction
-        draw_frame(canvas, row, column, frame_2)
-        await asyncio.sleep(0)
-
-        draw_frame(canvas, row, column, frame_2, negative=True)
+            draw_frame(canvas, row, column, frame, negative=True)
 
 
 async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
@@ -115,10 +75,6 @@ def choose_star():
   return random.choice(stars)
 
 
-def choose_coords(maxx, maxy):
-  return random.randint(1, maxx-1), random.randint(1, maxy-1)
-
-
 def main(canvas):
     canvas.border()
     canvas.nodelay(True)
@@ -126,24 +82,28 @@ def main(canvas):
     stars_count = 100
     coroutines = []
     max_row, max_column = canvas.getmaxyx()
+    space_near_border = 2
     for _ in range(stars_count):
         coroutines.append(blink(
             canvas,
-            random.randint(2, max_row-2),
-            random.randint(2, max_column-2),
+            random.randint(space_near_border, max_row-space_near_border),
+            random.randint(space_near_border, max_column - space_near_border),
             symbol=choose_star()
         ))
-    x = 0
     frame_1 = get_frame('./animations/rocket_frame_1.txt')
     frame_2 = get_frame('./animations/rocket_frame_2.txt')
     spaceship_width, spaceship_height = get_frame_size(frame_1)
+    rows_center = max_row//2-spaceship_width//2
+    columns_center = max_column//2 - spaceship_height//2
     spaceship = animate_spaceship(canvas,
                                   frame_1,
                                   frame_2,
-                                  (max_row//2-spaceship_width//2),
-                                  (max_column//2 - spaceship_height//2))
-    while x<200:
-        x += 1
+                                  rows_center,
+                                  columns_center)
+    loop = 0
+    loops_count = 100
+    while loop < loops_count:
+        loop += 1
         for _ in range(stars_count):
             coroutine = random.choice(coroutines)
             coroutine.send(None)
